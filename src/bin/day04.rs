28b49b85c4c6580c53be713f47e25984823e02a4
Christpoher Tay <christopher.tay@vidimensions.com>
2020-01-08 14:54:28 +0800

    Simpler alternative to parse and sort for day04

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,57 +1,47 @@
 // #[macro_use]
 // extern crate lazy_static;
-extern crate regex;
+// extern crate regex;
 
-use regex::Regex;
+// use regex::Regex;
 // use std::collections::HashMap;
-// use core::str::FromStr;
-// use std::result;
-// use std::error::Error;
+use core::str::FromStr;
+use std::result;
+use std::error::Error;
 
-// type Result<T> = result::Result<T, Box<dyn Error>>;
+type Result<T> = result::Result<T, Box<dyn Error>>;
 
-// #[derive(Debug)]
-// enum LogType { Start, Sleep, Wake }
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-struct LogEntry
+#[derive(Debug)]
+enum LogEntry
 {
-    month: i8,
-    day: i8,
-    hour: i8,
-    min: i8,
-    id: i16,
-    sleep: bool
+    NewGuard(u16), Wake(u8), Sleep(u8)
 }
 
+impl FromStr for LogEntry 
+{
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<LogEntry>
+    {
+        if s.contains("Guard") { 
+            let pos = match s.find(" b") { Some(x) => x, _ => Err("Error parsing Gaurd #")? };
+            return Ok(LogEntry::NewGuard(s[26..pos].parse()?)) 
+        }
+
+        let min: u8 = s[15..17].parse()?;
+        if s.contains("falls") { return Ok(LogEntry::Wake(min)); }
+        if s.contains("wakes") { return Ok(LogEntry::Sleep(min)); }
+        return Err("Cannot parse LogEntry")?;
+    }
+}
 
 fn main()
 {
     let dat = include_str!("Day04.txt");
-    let re = Regex::new(r"(?x)
-        \[ 1518 - (?P<month>\d+) - (?P<day>\d+) \s (?P<hour>\d+) : (?P<min>\d+) \] \s
-        (?:Guard\s\#)? (?P<type>(falls|wakes|\d+))
-        ").unwrap();
+    let mut dat_vec: Vec<&str> = dat.lines().collect();
+    dat_vec.sort();
 
-    let mut guard_logs = Vec::new();
-    for cap in re.captures_iter(dat) {
-        let id: i16 = match &cap["type"] {
-            "falls"|"wakes" => -1,
-            num => num.parse().unwrap()
-        };
-        guard_logs.push(LogEntry{
-            month: cap["month"].parse().unwrap(),
-            day: cap["day"].parse().unwrap(),
-            hour: cap["hour"].parse().unwrap(),
-            min: cap["min"].parse().unwrap(),
-            id: id, 
-            sleep: match &cap["type"] {
-                "falls"=> true,
-                _ => false
-            }
-        });
+    let logs: Vec<LogEntry> = dat_vec.iter().map(|s| s.parse().unwrap() ).collect();
+    for d in logs {
+        println!("{:?}", d);
     }
-    guard_logs.sort();  
-
-    println!("{:?}", guard_logs);
 }
