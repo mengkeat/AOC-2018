348df17e6d87f7daefe0c21884f4746b940c9c66
Christpoher Tay <christopher.tay@vidimensions.com>
2020-01-09 13:07:20 +0800

    Day 6 part 1

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -12,12 +12,37 @@ fn get_points(s: &str) -> Vec<Pt>
      }).collect();
 }
 
+fn part1(pts: &Vec<Pt>, x_max: i32, y_max: i32) -> i32
+{
+    let mut area = vec![0; pts.len()];
+
+    for x in 0..x_max {
+        for y in 0..y_max {
+            let dist_to_pts: Vec<i32> = pts.iter().map(|p| m_dist(*p, (x,y)) ).collect();
+            let min_dist: i32 = *dist_to_pts.iter().min().unwrap();
+            let min_pts: Vec<(usize, &i32)> = dist_to_pts.iter().enumerate().filter(|p| *p.1==min_dist ).collect();
+            if min_pts.len()==1 {
+                let i: usize = min_pts[0].0;
+                if x==0 || x==x_max-1 || y ==0 || y==y_max-1 {
+                    area[i] = -1;
+                }
+                else {
+                    area[i] += (area[i]>=0) as i32;
+                }
+            }
+        }
+    }
+
+    return *area.iter().max().unwrap();
+}
+
 fn main()
 {
     let dat = include_str!("Day06.txt");
     let pts = get_points(dat);
-    println!("{:?}", pts);
 
     let x_max = pts.iter().max_by_key(|p| p.0).unwrap().0;
     let y_max = pts.iter().max_by_key(|p| p.1).unwrap().1;
+
+    println!("Part 1: {}", part1(&pts, x_max, y_max));
 }
