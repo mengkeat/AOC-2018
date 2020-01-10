633bec685e70e9d49ef8342c7c76a194f8f5f365
Christpoher Tay <christopher.tay@vidimensions.com>
2020-01-10 17:32:57 +0800

    Day 8 part 1

---
+++ src/bin/day08.rs
@@ -0,0 +1,39 @@
+
+struct Node {
+    childs: Vec<Node>,
+    meta: Vec<u16>
+}
+
+fn make_node(d: &mut Vec<u16>) -> Node 
+{
+    let n_child = d.pop().unwrap();
+    let n_meta = d.pop().unwrap();
+
+    let mut childs = vec![];
+    for _ in 0..n_child {
+        childs.push(make_node(d));
+    }
+
+    let mut meta = vec![];
+    for _ in 0..n_meta {
+        meta.push(d.pop().unwrap());
+    }
+    return Node{ childs, meta };
+}
+
+fn sum_meta(n: &Node) -> u16
+{
+    return n.childs.iter().map(sum_meta).sum::<u16>() + n.meta.iter().sum::<u16>();
+}
+
+fn main()
+{
+    let dat: Vec<u16> = include_str!("Day08.txt").trim()
+                        .split(' ')
+                        .map(|s| s.parse().unwrap() )
+                        .collect();
+    let rev_dat: Vec<u16> = dat.into_iter().rev().collect();
+    let node = make_node(&mut rev_dat.clone());
+
+    println!("Part 1: {}", sum_meta(&node));
+}
