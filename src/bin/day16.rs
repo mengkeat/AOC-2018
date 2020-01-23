499cec5e72ba3662e1c346fe6c1483a3ae93b1a4
christopher <tay.christopher@gmail.com>
2020-01-23 23:22:44 +0800

    Day 16 part 1

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -4,40 +4,77 @@ use std::result;
 use std::error::Error;
 
 macro_rules! err {
-    ($($tt:tt)*) => { Err(Box::<Error>::from(format!($($tt)*))) }
+    ($($tt:tt)*) => { Err(Box::<dyn Error>::from(format!($($tt)*))) }
 }
 
-type Result<T> = result::Result<T, Box<Error>>;
+type Result<T> = result::Result<T, Box<dyn Error>>;
+
+#[derive(Clone, Debug, Copy)]
+enum OpCode { 
+    Addr, Addi,
+    Mulr, Muli,
+    Banr, Bani,
+    Borr, Bori,
+    Setr, Seti,
+    Gtir, Gtri, Gtrr,
+    Eqir, Eqri, Eqrr,
+}
 
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
-struct Registers([i64; 4]);
+struct Register([i64; 4]);
+// type Register = Vec<i64>;
 
 #[derive(Clone, Debug)]
 struct Sample {
-    before: Registers,
-    after: Registers,
+    before: Register,
+    after: Register,
     instr: Instruction,
 }
 
 #[derive(Clone, Debug)]
 struct Instruction {
-    Op: u8, A: u8, B: u8, C: u8,
+    op: u8, a: i64, b: i64, c: i64,
 }
 
 impl FromStr for Instruction {
-    type Err = Box<Error>;
+    type Err = Box<dyn Error>;
     
     fn from_str(s: &str) -> Result<Instruction> {
-        let v: Vec<u8>  = s.split_whitespace().map(|s| 
-                                s.trim().parse::<u8>().unwrap()
+        let v: Vec<i64>  = s.split_whitespace().map(|s| 
+                                s.trim().parse::<i64>().unwrap()
                             ).collect();
         if v.len()!=4 { 
             err!("Instruction is more than 4 numbers!") 
         }
         else {
-            Ok(Instruction{ Op: v[0], A: v[1], B: v[2], C: v[3]} )
+            Ok(Instruction{ op: v[0] as u8, a: v[1], b: v[2], c: v[3]} )
         }
     }
+}
+
+fn exe(reg: &Register, op_code: &OpCode, a: &i64, b: &i64, c: &i64) -> Register {
+    let mut r = reg.clone();
+    let r_a = r.0[*a as usize];
+    let r_b = r.0[*b as usize];
+    r.0[*c as usize] = match op_code {
+            OpCode::Addr  => r_a + r_b,
+            OpCode::Addi  => r_a + *b,
+            OpCode::Mulr  => r_a * r_b,
+            OpCode::Muli  => r_a * *b,
+            OpCode::Banr  => r_a & r_b,
+            OpCode::Bani  => r_a & *b,
+            OpCode::Borr  => r_a | r_b,
+            OpCode::Bori  => r_a | *b,
+            OpCode::Setr  => r_a,
+            OpCode::Seti  => *a,
+            OpCode::Gtir  => if *a>r_b {1} else {0},
+            OpCode::Gtri  => if r_a>*b {1} else {0},
+            OpCode::Gtrr  => if r_a>r_b {1} else {0},
+            OpCode::Eqir  => if *a==r_b {1} else {0},
+            OpCode::Eqri  => if r_a==*b {1} else {0},
+            OpCode::Eqrr  => if r_a==r_b {1} else {0},
+        };
+    return r;
 }
 
 fn parse(dat: &Vec<&str>) -> Result<Vec<Sample>> {
@@ -51,8 +88,8 @@ fn parse(dat: &Vec<&str>) -> Result<Vec<Sample>> {
             let af: Vec<i64> = ptr.next().unwrap()[9..19]
                                 .split(",").map(|s| s.trim().parse::<i64>().unwrap() ).collect();
             samples.push( Sample{ 
-                before: Registers([be[0], be[1], be[2], be[3]]),
-                after:  Registers([af[0], af[1], af[2], af[3]]),
+                before: Register( [be[0], be[1], be[2], be[3]] ),
+                after:  Register( [af[0], af[1], af[2], af[3]] ), 
                 instr: instr,
             });
             ptr.next();                                 
@@ -64,10 +101,38 @@ fn parse(dat: &Vec<&str>) -> Result<Vec<Sample>> {
     return Ok(samples);
 }
 
-fn main() 
-{
+fn compatible_ocode(s: &Sample) -> Vec<OpCode> {
+    let ocode_lst: Vec<OpCode> = vec![
+        OpCode::Addr, OpCode::Addi,
+        OpCode::Mulr, OpCode::Muli,
+        OpCode::Banr, OpCode::Bani,
+        OpCode::Borr, OpCode::Bori,
+        OpCode::Setr, OpCode::Seti,
+        OpCode::Gtir, OpCode::Gtri, OpCode::Gtrr,
+        OpCode::Eqir, OpCode::Eqri, OpCode::Eqrr,
+    ];
+    ocode_lst.iter()
+        .filter_map(|op| 
+            if exe(&s.before, op, &s.instr.a, &s.instr.b, &s.instr.c) == s.after {
+                Some(*op)
+            }
+            else { None }
+        )
+        .collect()
+}
+
+fn part1(samples: &Vec<Sample>) {
+    let n_samples = samples.iter()
+        .map(|s| compatible_ocode(s).len() )
+        .filter(|l| *l >= 3 )
+        .count();
+    println!("Part 1: {}", n_samples);
+}
+
+
+fn main() {
     let dat: Vec<&str> = include_str!("Day16.txt").lines().collect();
     let dat_samples = parse(&dat).unwrap();
 
-    println!("{:?}", dat_samples.len());
+    part1(&dat_samples);
 }
